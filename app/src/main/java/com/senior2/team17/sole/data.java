9f529af9a8a2939b169d302cdca28b44b2b63707
dyay108 <djboi94@gmail.com>
2016-03-22 13:28:46 -0400

    Edit

--- app/src/main/java/com/senior2/team17/sole/data.java
+++ app/src/main/java/com/senior2/team17/sole/data.java
@@ -3,24 +3,41 @@ package com.senior2.team17.sole;
 /**
  * Created by Dyay108 on 3/18/2016.
  */
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.UUID;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class data extends Activity {
 
-}
+public class data extends Fragment {
+
+
+    }
+
+
+
+
+
+
